e3912706b6cad532d31c70f7070fe606caee1378
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-07 13:27:11 +0530

    29 Practice set 6

---
+++ 29 Practice set 6/script.js
@@ -0,0 +1,78 @@
+console.log("Practice set 6");
+
+// Quetion 1 ----------------------->
+// make a drive calculator from taking input from user
+
+// alert("Hello this is driving calculator");
+// let a = prompt("Enter your age here");
+// a = Number.parseInt(a);
+// if (a>=18){
+//     console.log("you are eligible to drive");
+// }
+// else{
+//     console.log("you cannot drive");
+// }
+
+// Quetion 2 ----------------------->
+// use confirm to ask the user to see the prompt again in Que 1
+
+// alert("Hello this is driving calculator");
+// let runagain = true;
+
+// while (runagain){
+
+// let a = prompt("Enter your age here");
+// a = Number.parseInt(a);
+// if (a>=18){
+//     console.log("you are eligible to drive");
+// }
+// else{
+//     console.log("you cannot drive");
+// }
+// runagain = confirm("Do you want to check again");
+// }
+
+// Quetion 3 ----------------------->
+// in previous use clg error if age is negative
+
+// alert("Hello this is driving calculator");
+// let runagain = true;
+
+// while (runagain){
+
+// let a = prompt("Enter your age here");
+// a = Number.parseInt(a);
+// if (a<=0){
+//     console.error("Invalid age");
+//     console.log("please reload page to check again");
+//     break;
+// }
+// if (a>=18){
+//     console.log("you are eligible to drive");
+// }
+// else{
+//     console.log("you cannot drive");
+// }
+// runagain = confirm("Do you want to check again");
+// }
+
+
+// Quetion 4 ----------------------->
+// if user enter a number greater that 4 then change the url to google.com
+
+// alert("If you want to go on  google.com then enter a number greater than 4");
+
+// let number  = prompt("Enter a number");
+// number = Number.parseInt(number);
+
+// if (number > 4) {
+//     location.href = "https://google.com";
+// }
+
+
+// Quetion 5 ----------------------->
+// change background colour on the basis of user's input 
+
+let color = prompt("Enter colour of your choice");
+document.body.style.background = color; 
+
