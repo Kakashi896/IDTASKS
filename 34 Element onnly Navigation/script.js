4b5bef36d4034cb4cc1ee7f8d4c1b06adbb654a5
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-07 13:28:39 +0530

    /34 Element onnly Navigation

---
+++ 34 Element onnly Navigation/script.js
@@ -0,0 +1,6 @@
+const bgred = ()=>{
+    document.body.firstElementChild.style.background = "red";
+}
+let b = document.body;
+console.log("The first child of b is",b.firstChild);
+console.log("The Element first child of b is",b.firstElementChild);
