3737871e6b6838817c114eb64491d9c540762feb
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-07 13:28:02 +0530

    /32 Assecing children of element

---
+++ 32 Assecing children of element/script.js
@@ -0,0 +1,16 @@
+/* 
+
+<div>
+
+<p>   div ka phela beta  </p>
+<span>    div ka dusra beta aur p sa sibling    </span>
+
+
+</div>
+
+
+*/
+
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.childNodes);
