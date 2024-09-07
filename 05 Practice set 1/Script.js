817022e391e679110ae72929d3fcf7411580c06f
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-07 13:20:04 +0530

    05 Practice set 1

---
+++ 05 Practice set 1/Script.js
@@ -0,0 +1,45 @@
+ console.log('Practice');
+
+//  Quetion 1 -------------------------->
+// Create a variable of type string and try to add Node. into it ?
+
+let a = "this is string";
+let b = 9;
+
+console.log(a+b);
+
+//  Quetion 2 -------------------------->
+// use type of operator to find previous quetion datatype ?
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof (a+b));
+
+//  Quetion 3 -------------------------->
+// create a const object i javascript can you change it to hold a no. later ?
+
+const info = {
+      name:"akshat",
+      "emp id":"705285",
+}
+
+// info = 44  // can do (can you change it to hold a no. later)this in js.
+
+//  Quetion 4 -------------------------->
+// try to add a new key in info
+
+info["salary"] = "25LPA.."
+console.log(info);
+
+//  Quetion 5 -------------------------->
+// Write a js program to create a word meaning dictonary of 5 words
+
+const dictionary = {
+    maelstrom:"a restless, disordered, or tumultuous state of affairs",
+    splendiferous:"splendid; magnificent",
+    clepsydra:"an ancient device for measuring time by the regulated flow of water or mercury through a small aperture",
+    skedaddle:"to run away hurriedly; flee",
+    scrofulous:"morally tainted",
+}
+
+console.log(dictionary);
