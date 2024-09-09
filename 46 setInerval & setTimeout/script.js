50cc2f155c8f276c2d45145552cf8be17a5d4895
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-10 00:02:01 +0530

    /46 setInerval & setTimeout

---
+++ 46 setInerval & setTimeout/script.js
@@ -0,0 +1,33 @@
+document.write("See code in script.js");
+
+// let a = setTimeout(function(){
+//     alert("World   (i am late because i am in set timeout function)");
+// },5000);
+
+
+// let b = prompt("Do you want to run the set timeout----- type 'no' to clear it");
+// if ("no"  === b){
+//     clearTimeout(a);    //  to clear it
+// }
+
+
+// Another example
+
+// const sum  = (a,b, c) =>{
+//     console.log("i am running and the answer is " + (a + b + c));
+//     a + b + c;
+// }
+
+// setTimeout(sum ,1000 , 1, 2, 7);
+
+
+
+//  NOW SET INTERVAL
+
+// let i = setInterval (function(){
+//     alert("Set Interval hu mai chalta hi rahunga")
+// }, 4000);
+
+
+// to clear it 
+// clearInterval(i);
