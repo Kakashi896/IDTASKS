f5c617210e5d5623ffa5cf31e5a34605fd6c8fee
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-09-13 23:59:41 +0530

    89 Hosting

---
+++ 89 Hosting/script.js
@@ -0,0 +1,20 @@
+
+
+greet();    // this is not throw any error becuase hosting concept in js prevents it 
+
+function greet(){
+    console.log("Good morning");
+}
+
+// another concept  on var
+
+console.log(a); 
+var a = 9;
+console.log(a);
+
+
+// another concept  on let
+
+// console.log(b); // it will throw error 
+let b = 10;
+console.log(b);
